f3234b56231ef166c4534a85c3f16a636d12b2db
ryansummerlin <ryansummerlin777@gmail.com>
2023-05-14 17:31:39 -0700

    Created all static paths

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
-// Your code here
 
-const port = 5000;
+// Part 1
+// app.use(express.static('assets'));
+
+// Part 2
+// app.use('/scripts', express.static('assets/scripts'));
+
+// Part 3
+app.use('/stylesheets', express.static('assets/css'));
+
+const port = 5001;
 app.listen(port, () => console.log('Server is listening on port', port));
